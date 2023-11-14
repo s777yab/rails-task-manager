f8e423ff0570c92612f809b2966977df3378d276
Sayyab Khan <sayyabk05@gmail.com>
2023-11-14 15:49:58 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Show all tasks
+  get "tasks", to: "tasks#index"
+  # Create a new task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Edit a task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete a task
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
 end
